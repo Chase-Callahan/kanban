53e759af5c30b8aa8c993ff95abf3d996f994cda
Chase Callahan <chasetcallahan@gmail.com>
2026-02-20 16:13:26 -0600

    created terraform

---
+++ modules/integrations/github/project_management/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "5.41.0"
+    }
+  }
+}
+
+locals {
+  repository_name = "kanban"
+  github_owner    = "Chase-Callahan"
+}
+
+variable "milestones" {
+  type = map(object({
+    title       = string
+    due_date    = string
+    description = string
+  }))
+  description = "Milestones, consider them the biggest deliverable unit."
+}
+
+variable "labels" {
+  type = map(object({
+    name  = string
+    color = string
+  }))
+  description = "The labels to tag the issues."
+}
+
+provider "github" {
+  owner = local.github_owner
+}
+
+resource "github_repository" "kanban" {
+  name                   = local.repository_name
+  description            = "Taking the BEAM to production pragmatically."
+  visibility             = "private"
+  has_issues             = true
+  auto_init              = true
+  gitignore_template     = "Terraform"
+  delete_branch_on_merge = true
+}
+
+resource "github_repository_milestone" "epics" {
+  for_each    = var.milestones
+  owner       = local.github_owner
+  repository  = local.repository_name
+  title       = each.value.title
+  description = replace(each.value.description, "\n", " ")
+  due_date    = each.value.due_date
+  depends_on  = [github_repository.kanban]
+}
+
+resource "github_issue_label" "issues_labels" {
+  for_each   = var.labels
+  repository = local.repository_name
+  name       = each.value.name
+  color      = each.value.color
+  depends_on = [github_repository.kanban]
+}
---
+++ scripts/asdf_plugins.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# in scripts/asdf_plugins.sh
+# install necessary plugins
+
+plugins=(
+    "github-cli"
+    "packer"
+    "terraform"
+    "awscli"
+    "elixir"
+    "erlang"
+    "postgres"
+    "jq"
+    "age"
+    "sops"
+)
+
+for plugin in "${plugins[@]}"; do
+    # "|| true ignores errors when installing pre-existing plugins
+    asdf plugin-add "$plugin" || true
+done
+
+echo "Installation complete."
+echo "Please restart terminal or source profile file"
